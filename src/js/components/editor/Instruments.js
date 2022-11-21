6c6a63c1d67ad95d3774e5401466a8a9ae9dd708
Scott Horan <scottf.horan@gmail.com>
2022-11-20 21:08:45 -0500

    added the extend down feature to the instruments list

--- src/js/components/editor/Instruments.js
+++ src/js/components/editor/Instruments.js
@@ -1,9 +1,11 @@
-import React, { useId } from 'react'
+import React, { useId, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
     faGuitar,
     faBars,
     faHome,
+    faArrowUp,
+    faArrowDown,
 } from '@fortawesome/free-solid-svg-icons'
 import { motion, useDragControls } from 'framer-motion'
 
@@ -11,6 +13,7 @@ import Button from './Button'
 
 export default function Instruments() {
     const controls = useDragControls()
+    const [extendDown, setExtendDown] = useState(true)
 
     const buttons = [
         {label: 'Guitar', icon: faGuitar, id: useId(), images: null}
@@ -26,15 +29,28 @@ export default function Instruments() {
             <div className='editor-list-header'>
                 <FontAwesomeIcon icon={faBars} color='#707070' fontSize={18}/>
                 <FontAwesomeIcon icon={faHome} color='#707070' fontSize={18}/>
+                {extendDown ?
+                    <FontAwesomeIcon icon={faArrowUp} color='#707070' fontSize={18} onClick={() => setExtendDown(false)}/>
+                :
+                    <FontAwesomeIcon icon={faArrowDown} color='#707070' fontSize={18} onClick={() => setExtendDown(true)}/>
+                }
             </div>
-            {buttons.map((button,index) => (
-                <Button
-                    key={button.id}
-                    index={index}
-                    button={button}
-                    length={buttons.length}
-                />
-            ))}
+            {extendDown ?
+                <React.Fragment>
+                    {buttons.map((button,index) => (
+                        <Button
+                            key={button.id}
+                            index={index}
+                            button={button}
+                            length={buttons.length}
+                        />
+                    ))}
+                </React.Fragment>            
+        
+            :
+                <p className='editor-list-label'>Instruments</p>
+            }
+
         </motion.div>
     )
 }
