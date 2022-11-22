1c4e5c8e80460be9e92073bb637fd3e5ddb3806f
Scott Horan <scottf.horan@gmail.com>
2022-11-21 20:18:47 -0500

    updated the measure component with styleing, will need to expand to include notes outside of the current measure size

--- src/js/components/editor/Measure.js
+++ src/js/components/editor/Measure.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
 export default function Measure() {
+
+    const handleSelect = () => {
+
+
+
+    }
+
     return (
         <div className='editor-sheet-measure'>
-            <div className='editor-sheet-measure-line'>
-
-            </div>
-            <div className='editor-sheet-measure-line'>
-
-            </div>
-            <div className='editor-sheet-measure-line'>
-
-            </div>
-            <div className='editor-sheet-measure-line'>
-                
-            </div>
-        </div>
+            <div className='editor-sheet-measure-line'/>
+            <div className='editor-sheet-measure-space'/>
+            <div className='editor-sheet-measure-line'/>
+            <div className='editor-sheet-measure-space'/>
+            <div className='editor-sheet-measure-line'/>
+            <div className='editor-sheet-measure-space'/>
+            <div className='editor-sheet-measure-line'/>
+            <div className='editor-sheet-measure-space'/>
+            <div className='editor-sheet-measure-line'/>
+        </div> 
     )
 }
