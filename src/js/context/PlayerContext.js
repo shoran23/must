66bb341129f4c2f342fc3ae461d0d560b9884315
Scott Horan <scottf.horan@gmail.com>
2022-11-22 09:12:02 -0500

    started to map out the player context

--- src/js/context/PlayerContext.js
+++ src/js/context/PlayerContext.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Synth } from 'tone'
 
 const PlayerContext = React.createContext()
 
@@ -6,9 +7,31 @@ class PlayerProvider extends React.Component {
     constructor() {
         super()
         this.state = {
-
+            bpm: 60,
+            beat: 0,        // current beat within the player loop
+            music: {},
+            synth: new Synth().toDestination(),
         }
     }
+
+    handleState = (key, value) => {
+        this.setState({[key]: value})
+    }
+
+    start = () => {
+        // this will start the player loop with the provided music
+    }
+
+    stop = () => {
+        // this will stop the player loop with the provided music
+        // will reset the beat back to 0
+    }
+
+    pause = () => {
+        // this will stop the player loop with the provided music
+        // will not reset the beat to 0
+    }
+
     render() {
         return (
             <PlayerContext.Provider value={{...this.state}}>
